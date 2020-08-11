746ca806842da370a2a4b819b82090fd895e6c16
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-08-11 15:59:44 +0100

    remove commented code

--- plugin-wl-actionplan-widgets.php
+++ plugin-wl-actionplan-widgets.php
@@ -53,7 +53,7 @@ echo __( '', 'jr_widget_domain' );
 		// print_r ( get_user_favorites_list($user_id, $site_id, $include_links, $filters, $include_button, $include_thumbnails = false, $thumbnail_size = 'thumbnail', $include_excerpt = false) ) ;
 
 		// Print function (prints an html list): 
-		the_user_favorites_list($user_id, $site_id, $include_links = true, $filters, $include_button, $include_thumbnails = false, $thumbnail_size = 'thumbnail', $include_excerpt = false) ;
+		// the_user_favorites_list($user_id, $site_id, $include_links = true, $filters, $include_button, $include_thumbnails = false, $thumbnail_size = 'thumbnail', $include_excerpt = false) ;
 
 		// Print clear favourites button
 		the_clear_favorites_button($site_id, $text);
